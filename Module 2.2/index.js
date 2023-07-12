b418c52f62c04dd2ffcceebf5f1d146eddcf5d64
albayaaan <albayaaan.z@gmail.com>
2023-07-12 10:12:20 +0700

    init module 2.2

---
+++ Module 2.2/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const playlist = [
+    {
+        title: "Kill Bill",
+        artist: ["SZA", "Doja Cat"],
+        url: "https://open.spotify.com/track/0CrcwVLUe8wzLUIXgjXVZJ?si=326e2c46cc1344a5",
+    },
+    {
+        title: "Say You Won't Let Go",
+        artist: ["James Arthur"],
+        url: "https://open.spotify.com/track/1Pw5C4N6Fn5E4mGCxmbbVa?si=75ee9f9fbfa14614",
+    },
+];
+
+app.get("/", (req, res) => {
+    res.send("<h1>Spotify App</h1>");
+});
+
+app.get("/playlist", (req, res) => {
+    res.send(playlist);
+});
+
+app.post("/playlist", express.json(), (req, res) => {
+    playlist.push(req.body);
+    res.send(playlist);
+});
+
+app.get("/playlist/:id", (req, res) => {
+    let id = req.params.id;
+    res.redirect(playlist[id].url);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
