295bb79906fef4780fbd1fe3505b35fcd7a1eaef
albayaaan <albayaaan.z@gmail.com>
2023-07-12 20:40:41 +0700

    add API play

--- Module 2.2/index.js
+++ Module 2.2/index.js
@@ -3,11 +3,13 @@ const app = express();
 
 const playlist = [
     {
+        id: 1,
         title: "Kill Bill",
         artist: ["SZA", "Doja Cat"],
         url: "https://open.spotify.com/track/0CrcwVLUe8wzLUIXgjXVZJ?si=326e2c46cc1344a5",
     },
     {
+        id: 2,
         title: "Say You Won't Let Go",
         artist: ["James Arthur"],
         url: "https://open.spotify.com/track/1Pw5C4N6Fn5E4mGCxmbbVa?si=75ee9f9fbfa14614",
@@ -29,7 +31,24 @@ app.post("/playlist", express.json(), (req, res) => {
 
 app.get("/playlist/:id", (req, res) => {
     let id = req.params.id;
-    res.redirect(playlist[id].url);
+    let song = playlist.filter((song) => song.id == id);
+    if (song.length === 0) {
+        res.status(404).json({
+            msg: "data not found",
+        });
+    }
+    res.json(song);
+});
+
+app.get("/playlist/:id/play", (req, res) => {
+    let id = req.params.id;
+    let song = playlist.filter((song) => song.id == id);
+    if (song.length === 0) {
+        res.status(404).json({
+            msg: "data not found",
+        });
+    }
+    res.redirect(song[0].url);
 });
 
 app.listen(3000, () => {
