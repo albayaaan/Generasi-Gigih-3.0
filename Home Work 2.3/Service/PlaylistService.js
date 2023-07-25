d25e11088c66c0bb160a88c17ee0692cae7537a4
albayaaan <albayaaan.z@gmail.com>
2023-07-25 08:39:10 +0700

    edit

--- Home Work 2.3/Service/PlaylistService.js
+++ Home Work 2.3/Service/PlaylistService.js
@@ -1,27 +1,27 @@
-import playlist from "../Repository/Playlist.js";
+import playlistRepository from "../Repository/Playlist.js";
 
 // get all song
 const getAllSongs = () => {
-    const songs = playlist.all();
+    const songs = playlistRepository.all();
     const sortedSongs = songs.sort((a, b) => b.played - a.played); // sorted by most played
     return sortedSongs;
 };
 
 // get a song
 const getSong = (songId) => {
-    const song = playlist.find(songId);
+    const song = playlistRepository.find(songId);
     return song ?? false;
 };
 
 // add a song
 const addSong = (songData) => {
-    playlist.add(songData);
+    playlistRepository.add(songData);
     return;
 };
 
 // play a song
 const playSong = (songId) => {
-    const song = playlist.find(songId);
+    const song = playlistRepository.find(songId);
     if (song) {
         song.played += 1;
         return true;
